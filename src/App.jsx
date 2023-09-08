5fc708a687ff5954d0fcac93d4bacfed9ba16ed3
fishyboi@MSI <fishyboi@MSI>
2023-09-08 14:14:18 -0400

    Hello submit button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-
+import SignUpForm from './components/SignUpForm'
+import Authenticate from './components/Authenitcate'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>He He He Ha</h1>
+      <h1>A Form to Possibly Get a Free Something!!!</h1>
+      {SignUpForm()}
     </>
   )
 }
