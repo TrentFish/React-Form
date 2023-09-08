3b166452acc8883e5448c3d8eed74db26bb0bded
fishyboi@MSI <fishyboi@MSI>
2023-09-08 14:36:05 -0400

    Success: True

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -7,18 +7,38 @@ export default function SignUpForm() {
 
     async function handleSubmit(event) {
         event.preventDefault();
-        console.log("Hello 👋");
-      }
+      
+        try {
+            const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup', 
+            { 
+              method: "POST", 
+              headers: { 
+                "Content-Type": "application/json" 
+              }, 
+              body: JSON.stringify({ 
+                username: "", 
+                password: "" 
+              }) 
+            })
+            const result = await response.json();
+            //console.log(result);
+        } catch (error) {
+        setError(error.message);
+        }
+    }
 
     return (
         <>
             <h2>Sign Up!</h2>
+            {error && <p>{error}</p>}
             <form onSubmit={handleSubmit}>
                 <label>
-                    Username: <input value={username} onChange={(e) => setUsername(e.target.value)} />
+                    Username: {" "}
+                    <input value={username} onChange={(e) => setUsername(e.target.value)} />
                 </label>
                 <label>
-                    Password: <input value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    Password: {" "}
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </label>
                 <button>Submit</button>
             </form>
